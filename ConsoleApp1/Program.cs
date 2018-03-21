45d8c9877ae8607478c703a22567aa81d3c5a592
VINICIUS ALVES DE LIMA <01169867@sempreuninassau.com.br>
2018-03-21 10:17:52 -0300

    codigo de 10 porcento

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Código para soma de dois números
+
+            //Definindo variáveis
+            string a;
+            double Porcento;
+
+            // WriteLine é utilizado para escrito do texto no console
+            Console.WriteLine("Digite o valor que deseja calcular: ");
+            // ReadLine é utilizado para ler a variável digitada pelo usuário
+            a = Console.ReadLine();
+
+            Double.Parse(a);
+
+            double aNovo = Double.Parse(a);
+
+            Porcento = aNovo * 0.1;
+            Console.WriteLine("Resultado é :" + Porcento);
+            Console.ReadLine();
+            //Fim
+        }
+    }
+}
