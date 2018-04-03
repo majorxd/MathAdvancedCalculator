028e31de74dd4446cf701f098e025b17200ebdba
PEDRO HENRIQUE CÂNDIDO DOS SANTOS <01169866@sempreuninassau.com.br>
2018-04-03 11:57:57 -0300

    Fibonacci pronto.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp2
+class MainClass
 {
-	class Program
+	// Este código realiza a sequência de Fibonacci, e gera um novo número sempre que apertadoa tecla enter. 
+
+	public static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		// Criação das das variáveis.
+		int NumeroAnterior = 0;
+		int NumeroAtual = 1;
+		int NovoNumero;
+		int Fibonacci;
+
+		Console.WriteLine("Aperte Enter e um número será gerado na sequência de Fibonacci:");
+
+		// Estrutura de repetição.
+		for (int i = 0; i < 100; i++)
 		{
+			// A variável Fibonacci recebe o número anterior e soma com o número atual.
+			Fibonacci = NumeroAnterior + NumeroAtual;
+			// O console.WriteLine imprime no console.
+			Console.WriteLine(Fibonacci);
+			NumeroAnterior = NumeroAtual;
+			NumeroAtual = Fibonacci;
+			// O console.ReadKey 
+			Console.ReadKey();
+			//Fim do código.
 		}
+
 	}
 }
---
+++ Fibonacci1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci1
+{
+	class Program
+	{
+
+
+		public static void Main()
+		{
+			// Código para dizer o valor da sequência de  Fibonacci pela sua posição.
+			// WriteLina é utilizado para escrever o texto no console.
+			Console.WriteLine("Digite o termo ao qual você deseje o número Fibonacci: ");
+			int number = Convert.ToInt32(Console.ReadLine());
+
+
+			Console.WriteLine(Fibonacci(number));
+
+			// ReadKey é utilizado para esperar um enter finalizar o console.
+			Console.ReadKey();
+		}
+
+		static int Fibonacci(int x) // A variável x assume o valor da varíável number.
+		{
+			// Se x for menor ou igual a 1 o valor 1 será retornado .
+			if (x <= 1)
+			{
+				return 1;
+			}
+			// Se não retornará o número da sequência.
+			else
+			{
+				return Fibonacci(x - 1) + Fibonacci(x - 2);
+			}
+		}
+	}
+}
